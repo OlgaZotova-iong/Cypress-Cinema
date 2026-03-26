fc090c220506b0a7bb732563536512249fe13b04
OlgaZotova-iong <olgazotova197602@gmail.com>
2026-03-26 16:04:19 +0300

    Solving the second task

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '788h7m',
+
   e2e: {
     baseUrl: 'http://qamid.tmweb.ru',
     viewportWidth: 1920,
@@ -9,12 +11,20 @@ module.exports = defineConfig({
     screenshotOnRunFailure: true,
     defaultCommandTimeout: 10000,
     specPattern: "cypress/e2e/**/*.cy.js",
+
+    // Для версий Cypress 15.x иногда нужна явная настройка
+    setupNodeEvents(on, config) {
+      // Здесь можно добавлять плагины при необходимости
+      return config;
+    },
+
     retries: {
       runMode: 2,
       openMode: 0
     }
   }
 });
+
 
 
 
